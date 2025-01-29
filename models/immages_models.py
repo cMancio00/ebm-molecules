f2bbee75f2021d02e76263577747ccdb9694a9e8
Christian Mancini <80308799+cMancio00@users.noreply.github.com>
2025-01-29 10:59:46 +0100

    17 compile the model (#19)
    * Enabled TensorCore multiplications
    * Compiled the model for the energy
    
    Compilation is fast, since the model is tiny, but still halves epoch training time 11 it/s on a RTX A4000 vs 6 it/s
    
    ---------
    
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- models/immages_models.py
+++ models/immages_models.py
@@ -1,8 +1,9 @@
+import torch
 from torch import nn, sigmoid
 import lightning as pl
 
 class Swish(nn.Module):
-    
+
     def forward(self, x):
         return x * sigmoid(x)
     
@@ -29,7 +30,7 @@ class Small_CNN(pl.LightningModule):
                 Swish(),
                 nn.Linear(c_hid3, out_dim)
         )
-
+    @torch.compile(mode="reduce-overhead")
     def forward(self, x):
         x = self.cnn_layers(x).squeeze(dim=-1)
         return x
