4557ba247e679f2413ad5d489e9d8bc7d4d0dd22
Christian Mancini <mancinichristian00@gmail.com>
2025-03-22 16:32:39 +0000

    Added MoNet
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 from .immages_models import Small_CNN
 from .ebm import DeepEnergyModel
+from .graph_models import MoNet
 
-__all__  = ['Small_CNN', 'DeepEnergyModel']
+__all__  = ['Small_CNN', 'DeepEnergyModel', 'MoNet']
---
+++ models/graph_models.py
@@ -0,0 +1,42 @@
+from torch import nn, norm
+from torch_geometric.nn import max_pool, global_mean_pool
+from torch_geometric.nn.conv import GMMConv
+from torch_geometric.nn.pool import graclus
+import torch.nn.functional as F
+import torch_geometric.transforms as T
+from torch_geometric.utils import normalized_cut
+import lightning as pl
+
+
+def normalized_cut_2d(edge_index, pos):
+    row, col = edge_index
+    edge_attr = norm(pos[row] - pos[col], p=2, dim=1)
+    return normalized_cut(edge_index, edge_attr, num_nodes=pos.size(0))
+
+class MoNet(pl.LightningModule):
+    def __init__(self, kernel_size=3):
+        super(MoNet, self).__init__()
+        self.conv1 = GMMConv(1, 32, dim=2, kernel_size=kernel_size)
+        self.conv2 = GMMConv(32, 64, dim=2, kernel_size=kernel_size)
+        self.conv3 = GMMConv(64, 64, dim=2, kernel_size=kernel_size)
+        self.fc1 = nn.Linear(64, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, data):
+        data.x = F.elu(self.conv1(data.x, data.edge_index, data.edge_attr))
+        weight = normalized_cut_2d(data.edge_index, data.pos)
+        cluster = graclus(data.edge_index, weight, data.x.size(0))
+        data.edge_attr = None
+        data = max_pool(cluster, data, transform=T.Cartesian(cat=False))
+
+        data.x = F.elu(self.conv2(data.x, data.edge_index, data.edge_attr))
+        weight = normalized_cut_2d(data.edge_index, data.pos)
+        cluster = graclus(data.edge_index, weight, data.x.size(0))
+        data = max_pool(cluster, data, transform=T.Cartesian(cat=False))
+
+        data.x = F.elu(self.conv3(data.x, data.edge_index, data.edge_attr))
+
+        x = global_mean_pool(data.x, data.batch)
+        x = F.elu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        return F.log_softmax(self.fc2(x), dim=1)
