d66d12883deb354cf9c9c4f3e0e4d00bf4b6e743
Christian Mancini <mancinichristian00@gmail.com>
2025-03-22 15:56:48 +0000

    Added MNISTSuperPixel DataModule
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

---
+++ DataModules/MNISTSuperpixelDataModule.py
@@ -0,0 +1,50 @@
+from typing import override
+import lightning as pl
+from torch.utils.data import random_split
+from torch_geometric.loader import DataLoader
+from torch_geometric.datasets import MNISTSuperpixels
+import torch_geometric.transforms as T
+
+
+class MNISTSuperpixelDataModule(pl.LightningDataModule):
+
+    def __init__(self, data_dir: str = "./datasets/MNISTSuperpixel", batch_size: int = 32, num_workers: int = 4):
+        super().__init__()
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.mnist_train = None
+        self.mnist_val = None
+        self.mnist_test = None
+
+    @override
+    def prepare_data(self):
+        # Better check why we use Polar coordinates
+        MNISTSuperpixels(self.data_dir, train=True, pre_transform=T.Polar())
+        MNISTSuperpixels(self.data_dir, train=False, pre_transform=T.Polar())
+
+    @override
+    def setup(self, stage):
+        if stage == "fit":
+            mnist_full = MNISTSuperpixels(self.data_dir, train=True)
+            # to_take = mnist_full.targets == 0
+            # mnist_full.data = mnist_full.data[to_take]
+            # mnist_full.targets = mnist_full.targets[to_take]
+            self.mnist_train, self.mnist_val = random_split(
+                mnist_full, [11/12, 1/12]
+            )
+        if stage == "test":
+            self.mnist_test = MNISTSuperpixels(self.data_dir, train=False)
+
+    @override
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=self.batch_size, drop_last=True, shuffle=True, pin_memory=True,
+                          num_workers=self.num_workers)
+
+    @override
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    @override
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=self.batch_size, num_workers=self.num_workers)
--- DataModules/__init__.py
+++ DataModules/__init__.py
@@ -1,4 +1,5 @@
 from .MNISTDataModule import MNISTDataModule
 from .FashionMNISTDataModule import FashionMNISTDataModule
+from .MNISTSuperpixelDataModule import MNISTSuperpixelDataModule
 
-__all__ = ["MNISTDataModule", 'FashionMNISTDataModule']
+__all__ = ["MNISTDataModule", 'FashionMNISTDataModule', 'MNISTSuperpixelDataModule']
