37a70147230d2d5493d8455ed5fc7043e599caff
Christian Mancini <mancinichristian00@gmail.com>
2025-04-25 13:45:03 +0000

    Removed fashion MNIST
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- DataModules/__init__.py
+++ DataModules/__init__.py
@@ -1,5 +1,4 @@
 from .MNISTDataModule import MNISTDataModule
-from .FashionMNISTDataModule import FashionMNISTDataModule
 from .MNISTSuperpixelDataModule import MNISTSuperpixelDataModule
 
-__all__ = ["MNISTDataModule", 'FashionMNISTDataModule', 'MNISTSuperpixelDataModule']
+__all__ = ["MNISTDataModule", 'MNISTSuperpixelDataModule']
