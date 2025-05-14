8bee3159f0ef7cfade0897bfead28a18ed1bac71
Castellana <danielecastellana22@gmail.com>
2025-05-14 19:12:49 +0200

    minor changes

--- images_exp.py
+++ images_exp.py
@@ -1,10 +1,7 @@
 import torch
 from ebm import DeepEnergyModel
-from data_modules import MNISTDataModule
 from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
-from utils.callbacks import BufferSamplerCallback
 from lightning.pytorch.cli import LightningCLI
-from lightning.pytorch.loggers import TensorBoardLogger
 
 torch.set_float32_matmul_precision('high')
 
@@ -16,30 +13,23 @@ torch.backends.cudnn.benchmark = False
 class ImagesLightningCLI(LightningCLI):
 
     def __init__(self, *args, **kwargs):
-        super().__init__(model_class=DeepEnergyModel, datamodule_class=MNISTDataModule, *args, **kwargs)
+        super().__init__(model_class=DeepEnergyModel, *args, **kwargs)
 
     def after_instantiate_classes(self) -> None:
         self.model.sampler.num_classes = self.datamodule.num_classes
         self.model.sampler.img_shape = self.datamodule.img_shape
 
 
-def cli_main():
+if __name__ == '__main__':
 
     cli = ImagesLightningCLI(
         seed_everything_default=42,
         trainer_defaults={
             'callbacks': [
                 ModelCheckpoint(save_top_k=1,auto_insert_metric_name=True,
-                                monitor='val_contrastive_divergence'),
+                                monitor="Accuracy Validation"),
                 LearningRateMonitor("epoch")
             ]
         }
 
     )
-
-    cli.trainer.logger = TensorBoardLogger("lightning_logs")
-
-
-if __name__ == '__main__':
-    cli_main()
-
