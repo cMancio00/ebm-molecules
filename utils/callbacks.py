93409e9987f60019b17a9e9d41531d39d62c316e
Castellana <danielecastellana22@gmail.com>
2025-05-14 20:15:18 +0200

    update GenerateCallback to correctly plot samples during the generation

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -12,13 +12,13 @@ from torchvision.utils import make_grid
 
 class GenerateCallback(pl.Callback):
 
-    def __init__(self, vis_steps: int=10, every_n_epochs: int=5):
+    def __init__(self, n_plot_during_generation: int=10, every_n_epochs: int=5):
         """Uses MCMC to sample tensors from the model and logs them in the Tensorboard at the end
         of training epochs.
 
         Args:
             num_steps (int, optional): Number of MCMC steps to take during generation. Defaults to 256.
-            vis_steps (int, optional): Steps within generation to visualize. Defaults to 8.
+            n_plot_during_generation (int, optional): Number of samples to visualize during the generation. Defaults to 10.
             every_n_epochs (int, optional): When we want to generate tensors. Defaults to 5.
             tensors_to_generate (int, optional): Number of tensors to generate. Defaults to 1
         
@@ -26,7 +26,7 @@ class GenerateCallback(pl.Callback):
         visualize 1 image each 32 steps (256/8).
         """
         super().__init__()
-        self.vis_steps = vis_steps
+        self.n_plot_during_generation = n_plot_during_generation
         self.every_n_epochs = every_n_epochs
 
     def on_train_epoch_end(self, trainer: Trainer, pl_module: LightningModule):
@@ -38,12 +38,27 @@ class GenerateCallback(pl.Callback):
         """
         if trainer.current_epoch % self.every_n_epochs == 0:
             num_classes = pl_module.sampler.num_classes
-            samples_to_plot = pl_module.sampler.MCMC_generation(model=pl_module.nn_model,
-                                                                steps=pl_module.hparams.mcmc_steps_gen,
-                                                                step_size=pl_module.hparams.mcmc_learning_rate_gen,
-                                                                labels=torch.arange(num_classes).to(pl_module.device))
-            imgs_to_plot = [pl_module.sampler.plot_sample(s) for s in samples_to_plot]
-            grid = torchvision.utils.make_grid(imgs_to_plot, nrow=10)
+            device = pl_module.device
+            labels = torch.arange(num_classes, device=device)
+
+            start_x, _ = pl_module.sampler.generate_random_batch(batch_size=labels.shape[0], device=device, collate=True)
+            all_sample = [pl_module.sampler.plot_sample(start_x).clone()]
+
+            n_steps = pl_module.hparams.mcmc_steps_gen
+
+            mcmc_steps = n_steps // self.n_plot_during_generation
+
+            for _ in range(self.n_plot_during_generation):
+
+                start_x = pl_module.sampler.MCMC_generation(model=pl_module.nn_model,
+                                                            steps=mcmc_steps,
+                                                            step_size=pl_module.hparams.mcmc_learning_rate_gen,
+                                                            labels=labels,
+                                                            starting_x=start_x)
+                all_sample.append(pl_module.sampler.plot_sample(start_x).clone())
+
+            all_images = torch.stack(all_sample, dim=1).view((-1,) + all_sample[0].shape[1:])
+            grid = torchvision.utils.make_grid(all_images, nrow=self.n_plot_during_generation+1)
             trainer.logger.experiment.add_image(f"Generation during Training", grid, global_step=trainer.current_epoch)
 
 
