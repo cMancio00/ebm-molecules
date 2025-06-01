b7d0dee270c1b2a2f81e54a10a1e68aaac790503
Christian Mancini <mancinichristian00@gmail.com>
2025-06-01 19:07:20 +0200

    Removed ComputeSmiles callback

--- callbacks/others.py
+++ callbacks/others.py
@@ -20,28 +20,3 @@ class SpectralNormalizationCallback(pl.Callback):
             if hasattr(module, "weight") and ("weight" in dict(module.named_parameters())):
                 if not is_parametrized(module, "weight"):
                     spectral_norm(module, name="weight", n_power_iterations=1)
-
-
-
-class ComputeSmilesOnTrainingDatasetCallback(pl.Callback):
-    def __init__(self):
-        super().__init__()
-
-    def _compute_smiles(self, train_dataloader) -> Set:
-        smiles = set()
-        for data, _ in iter(train_dataloader):
-            batch_size = data.x.shape[0]
-            for i in range(batch_size):
-                x = data[i].x
-                adj = data[i].adj
-                mask = data[i].mask
-
-                mol = to_rdkit_mol(x, adj, mask)
-                smiles.add(Chem.MolToSmiles(mol, isomericSmiles=True))
-        return smiles
-
-    def on_train_start(self, trainer: Trainer, model: LightningModule):
-        if not isinstance(model.sampler, MolSampler):
-            raise ValueError("Only MolSampler can have SMILES set")
-
-        model.sampler.smile_set = self._compute_smiles(trainer.train_dataloader)
