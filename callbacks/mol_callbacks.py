9ad9ac96611971e977ac5dbbce29f9da58f1b5bb
Christian Mancini <mancinichristian00@gmail.com>
2025-06-03 10:52:49 +0200

    Fixed mol_metrics to be printed in the test summary

--- callbacks/mol_callbacks.py
+++ callbacks/mol_callbacks.py
@@ -9,15 +9,16 @@ class ComputeMolMetricsCallback(Callback):
         super().__init__()
         self.mol_to_generate = mol_to_generate
 
-    def on_test_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
+    def on_test_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
         mols = pl_module.generate_samples(self.mol_to_generate)
 
         if not isinstance(pl_module.sampler, MolSampler):
             raise ValueError(f"{pl_module.sampler} is not a MolSampler")
         uniqueness = Uniqueness()
         uniqueness.update(generated_batch=mols, batch_size=self.mol_to_generate)
-        trainer.logger.experiment.add_scalar("uniqueness/test", uniqueness.compute())
+        # trainer.logger.experiment.add_scalar("uniqueness/test", uniqueness.compute())
+        pl_module.log("uniqueness/test", uniqueness.compute())
 
         novelty = Novelty()
         novelty.update(generated_batch=mols, training_smiles=trainer.datamodule.training_smiles, batch_size=self.mol_to_generate)
-        trainer.logger.experiment.add_scalar("novelty/test", novelty.compute())
+        pl_module.log("novelty/test", novelty.compute())
